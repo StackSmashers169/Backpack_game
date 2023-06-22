9663cbaf98741c729164e535638c04572e636feb
VicJo <VicJohnathanWilson@outlook.com>
2023-06-22 23:33:52 +0800

    Starting test cases

---
+++ test/test.py
@@ -0,0 +1,11 @@
+from src.player import Player
+from src.game import Game
+from src.location import Location
+from src.world import World
+from src.item import Item
+from src.npc import NPC
+
+import unittest
+
+
+class TestStack(unittest.TestCase):
