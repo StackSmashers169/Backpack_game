92cc471c9313f75ceb4f12da1aec8d9bbaa6b67a
VicJo <VicJohnathanWilson@outlook.com>
2023-06-23 08:44:34 +0800

    Setting up Test Cases

--- test/test.py
+++ test/test.py
@@ -9,3 +9,8 @@ import unittest
 
 
 class TestStack(unittest.TestCase):
+    def setup(self):
+        test_player = Player("Test")
+        test_world = World()
+        test_game = Game(test_world, test_player)
+
